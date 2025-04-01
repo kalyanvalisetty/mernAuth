8cefb2407dc93f5627954dfa1831144d570c9597
Kalyan Valisetty <139623608+kalyanvalisetty@users.noreply.github.com>
2025-04-01 22:04:32 +0530

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -2,11 +2,13 @@ import jwt from "jsonwebtoken";
 
 const userAuth = async(req,res,next)=>{
     const {token} = req.cookies;
+    console.log(token)
     if(!token){
         return res.json({success: false, message: "Not Authorized - Please Login Again"})
     }
     try{
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(tokenDecode.id)
         if(!tokenDecode.id){
             return res.json({success: false, message: "Not Authorized - Please Login Again"})
         }
